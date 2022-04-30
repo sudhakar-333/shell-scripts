d78e1fd5ad207dda4e6f111faa81fa2a25cc8625
sudhakar-333 <sudhakarthota33@gmail.com>
2022-04-30 23:42:29 +0530

    update_sh

---
+++ script2.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+command1="uname -a"
+command2="unaame -a"
+command3="usersadd ram"
+echo "command status for 3 variables"
+a=$($command1)
+echo "status code for command1: $?"
+b=$($command2)
+echo "status code for command2: $?"
+c=$($command3)
+echo "status code for command3: $?"
